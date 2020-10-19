84d138cda74a479e3cae9aa78a285408592d3096
youknow_zach <zachary.stambaugh@gmail.com>
2020-10-19 15:54:52 -0500

    original program to practice python. code operation in file comments

---
+++ pullParseSetDB.py
@@ -0,0 +1,6 @@
+# Pull from URL: http://py4e-data.dr-chuck.net/comments_917124.json
+# break out the JSON information
+# link up users with their ID numbers
+# many-to-many database linking users with the letters in their names
+
+print("Test")
