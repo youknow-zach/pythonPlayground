c6bb68a458f813ba517aa5be1b8aaf089460039d
youknow_zach <zachary.stambaugh@gmail.com>
2020-10-24 22:30:55 -0500

    Working on parse DB - it now pulls json data from the site

--- pullParseSetDB.py
+++ pullParseSetDB.py
@@ -3,4 +3,37 @@
 # link up users with their ID numbers
 # many-to-many database linking users with the letters in their names
 
-print("Test")
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    url = input('Enter URL or quit: ')
+    if len(url) < 1: url = 'http://py4e-data.dr-chuck.net/comments_917124.json'
+    elif url == 'quit': quit()
+
+    print('Retrieving', url)
+    try:
+        uh = urllib.request.urlopen(url, context=ctx)
+        data = uh.read().decode()
+    except:
+        print('==== Not a valid URL. Try again ====')
+        continue
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js:
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
+
+    print(json.dumps(js, indent=4))
