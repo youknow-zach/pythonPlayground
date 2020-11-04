ea473d0edab2b762ce92ff8439b94f8fde376bea
youknow_zach <zachary.stambaugh@gmail.com>
2020-11-04 09:21:35 -0600

    Changed test script to scripted example for setting up and utilizing classes in Python

--- GitTest.py
+++
@@ -1,2 +0,0 @@
-#Testing
-print("Hello Zach!")
---
+++ MakingClasses.py
@@ -0,0 +1,22 @@
+#S etting up a basic class
+class MyClass:
+
+    # Inherent variable in the class
+    variable = 'blah'
+
+    # Inherent function in the class
+    def function(self):
+        print('This is a message inside the class')
+
+# Creating instances of MyClass objects
+myobjectx = MyClass()
+myobjecty = MyClass()
+
+# Overwriting the Inherent variable in myobjecty object instance
+myobjecty.variable = 'yackity'
+
+# Then print out both values
+myobjectx.function()
+myobjecty.function()
+print(myobjectx.variable)
+print(myobjecty.variable)
